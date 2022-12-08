cb91143da19511c41a675dda1586583022fb8760
elias-py <eliascaceresvaldez837@gmail.com>
2022-12-07 22:32:54 -0300

    Upload Image functionality

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -92,4 +92,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Recipe Detail Serializer Class."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer to upload image."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,9 +3,11 @@ Views for recipe API.
 """
 from rest_framework import (
     viewsets,
-    mixins
+    mixins,
+    status,
 )
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from recipe import serializers
 from core.models import (
     Recipe,
@@ -31,12 +33,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create new Recipe."""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(
